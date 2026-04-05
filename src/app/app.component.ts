3af7ba8aca0364dacf3658e0c5b09660ea06dee3
rali-dev <ali.saee@outlook.de>
2026-04-05 13:18:20 +0200

    2. on way binding, two way bindings, FormsModule, [(FormModule)], (click), (mouseenter)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true
 })
 export class AppComponent {
-  title = 'todo-app';
+  public readonly name: string = 'Ali Rahimi';
+  public readonly id: string = 'el-1';
+
+  public handleClick(): void {
+    alert(`Hello, ${this.name}`);
+  }
+ 
+  public handleMouseEnter(e: MouseEvent): void {
+    console.log('Mouse entered:', e);
+  }
 }
